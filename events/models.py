b6c7a80a0f80676acac797ce5aebe712fb3c59f2
ramlakhan12 <ramlakhan78788@gmail.com>
2024-11-23 21:51:25 +0530

    Initial commit

---
+++ events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    #__cpLocation = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
---
+++ events/views.py
@@ -0,0 +1,41 @@
+# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Event
+from .forms import EventForm
+
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'events/event_detail.html', {'event': event})
+
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm()
+    return render(request, 'events/event_form.html', {'form': form, 'title': 'Add Event'})
+
+def update_event(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'events/event_form.html', {'form': form, 'title': 'Update Event'})
+
+def delete_event(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('event_list')
+    return render(request, 'events/event_confirm_delete.html', {'event': event})
